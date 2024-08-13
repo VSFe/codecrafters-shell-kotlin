483b13eb340cd41ab4a0641ab6ad0cada300831b
vsfe <klm03025@gmail.com>
2024-08-14 02:23:08 +0900

    feat: step 7
    - PATH를 사용한 type 명령어 개선

--- src/main/kotlin/command/Type.kt
+++ src/main/kotlin/command/Type.kt
@@ -1,7 +1,22 @@
 package command
 
+import java.io.File
+
 fun runType(args: List<String>) {
-    BuiltinShellCommand.entries.find { it.command == args[0] } ?.let {
+    BuiltinShellCommand.entries.firstOrNull { it.command == args[0] } ?.let {
         println("${it.command} is a shell builtin")
+        return
+    }
+
+    resolvePath(args[0]) ?.let {
+        println("${args[0]} is $it")
     } ?: println("${args[0]}: not found")
+}
+
+private fun resolvePath(arg: String): String? {
+    return System.getenv("PATH")
+        ?.split(":")
+        ?.map { File(it, arg) }
+        ?.firstOrNull { it.exists() }
+        ?.absolutePath
 }
