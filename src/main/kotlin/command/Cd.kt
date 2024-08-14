72f0506a4cb18764fe6334ae28f21eacfb0710fa
vsfe <klm03025@gmail.com>
2024-08-14 22:48:59 +0900

    feat: Navigation - step 3 & 4
    - cd: relativePath 구현

--- src/main/kotlin/command/Cd.kt
+++ src/main/kotlin/command/Cd.kt
@@ -3,9 +3,20 @@ package command
 import path.PathHolder
 
 fun runCd(args: List<String>) {
-    if (!args[0].startsWith(".")) {
-        if (!PathHolder.changeAbsolutePath(args[0])) {
-            println("cd: ${args[0]}: No such file or directory")
+    when {
+        args[0].startsWith(".") -> {
+            if (!PathHolder.changeRelativePath(args[0])) {
+                println("cd: ${PathHolder.getCanonicalPath(args[0])}: No such file or directory")
+            }
+        }
+        args[0] == "~" -> {
+            val path = System.getenv("HOME")
+            PathHolder.changeAbsolutePath(path)
+        }
+        else -> {
+            if (!PathHolder.changeAbsolutePath(args[0])) {
+                println("cd: ${args[0]}: No such file or directory")
+            }
         }
     }
 }
--- src/main/kotlin/path/PathHolder.kt
+++ src/main/kotlin/path/PathHolder.kt
@@ -14,4 +14,18 @@ object PathHolder {
         }
         return false
     }
+
+    fun changeRelativePath(relativePath: String): Boolean {
+        val canonicalPath = getCanonicalPath(relativePath)
+        if (File(canonicalPath).exists()) {
+            path = canonicalPath
+            return true
+        }
+        return false
+    }
+
+    fun getCanonicalPath(relativePath: String): String {
+        val newPath = if (path.endsWith("/")) "$path$relativePath" else "$path/$relativePath"
+        return File(newPath).canonicalPath
+    }
 }
